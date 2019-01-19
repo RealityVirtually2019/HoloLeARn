1bb13297d7b974fa627c279e5468aab7c1e1a2c8
sycrus <jzhchung@gmail.com>
2019-01-18 23:03:45 -0500

    Added boy animations

---
+++ Assets/Scripts/DanceTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceTrigger : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Dance");
+        }
+    }
+}
