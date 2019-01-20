f81e412d8d8f936f576f19656549a6b340487b7e
sycrus <jzhchung@gmail.com>
2019-01-19 21:32:58 -0500

    Added animations, controller and audio scripts

---
+++ Assets/Joe/PlayAudio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudio : MonoBehaviour {
+
+    public AudioClip otherClip;
+
+    IEnumerator Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = otherClip;
+        audio.Play();
+    }
+}
